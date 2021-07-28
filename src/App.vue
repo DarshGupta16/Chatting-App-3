47837c5dc2b77fd85132792a447b2c057e3c93bc
Darsh Gupta <darsh.gupta.1602@gmail.com>
2021-07-28 09:50:08 +0530

    Add basic HTML and styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <center>
+    <h1>The Chatters | By Darsh Gupta</h1>
+    <br />
+    <div class="chatbox">
+      <div class="chat" id="chat" style="text-align:left;">
+        <div>
+          <p class="sender">
+            <strong></strong>
+          </p>
+          <p class="message"></p>
+        </div>
+      </div>
+      <form class="input-flexbox">
+        <input type="text" class="form-control" v-model="messageInput" />
+        <button class="btn btn-primary">
+          Add Message
+        </button>
+      </form>
+    </div>
+  </center>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
+@import url("./assets/styles/style.css");
 </style>
---
+++ src/Signup.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Signup</h1>
+  <button class="btn btn-primary">Go Back</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
