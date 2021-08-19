e6f4349f88399a938c74369559530728699c6624
Darsh Gupta <darsh.gupta.1602@gmail.com>
2021-08-19 09:22:18 +0530

    Add change email and password features

--- src/SettingsModal.vue
+++ src/SettingsModal.vue
@@ -3,10 +3,26 @@
     <h1>Settings</h1>
     <form>
       <h2>Change Username</h2>
-      {{ error }}
+      {{ usernameerror }}
       <input type="text" class="form-control" v-model="username" />
       <button class="btn btn-primary" type="submit" @click="changeusername">
         Change Username
+      </button>
+    </form>
+    <br />
+    <form>
+      {{ emailerror }}
+      <input type="email" class="form-control" v-model="emailaddress" />
+      <button type="submit" class="btn btn-primary" @click="changeemail">
+        Change Email
+      </button>
+    </form>
+    <br />
+    <form>
+      {{ passworderror }}
+      <input type="password" class="form-control" v-model="password" />
+      <button type="submit" class="btn btn-primary" @click="changepassword">
+        Change Password
       </button>
     </form>
     <br />
@@ -26,7 +42,11 @@ export default {
   data() {
     return {
       username: "",
-      error: "",
+      usernameerror: "",
+      emailaddress: "",
+      emailerror: "",
+      passworderror: "",
+      password: "",
     };
   },
   methods: {
@@ -56,9 +76,31 @@ export default {
               this.username = "";
             });
         } else {
-          this.error = "Username taken. Please try another.";
+          this.usernameerror = "Username taken. Please try another.";
         }
       });
+    },
+    changeemail(e) {
+      e.preventDefault();
+      const user = firebase.auth().currentUser;
+      user
+        .updateEmail(this.emailaddress)
+        .then(() => {
+          alert("Email updated!!");
+          this.$emit("closemodal");
+        })
+        .catch((error) => (this.emailerror = error.message));
+    },
+    changepassword(e) {
+      e.preventDefault();
+      const user = firebase.auth().currentUser;
+      user
+        .updatePassword(this.password)
+        .then(() => {
+          alert("Changed Password!");
+          this.$emit("closemodal");
+        })
+        .catch((error) => (this.passworderror = error.message));
     },
   },
 };
